38646cd79cdff74a22c1bdb7f88df2f503d546ad
정우제 <111894594+wjddnwp29@users.noreply.github.com>
2025-08-09 18:03:16 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+
+
+int main(){
+    cout << "\\    /\\" <<'\n';
+    cout << " )  ( ') " <<'\n';
+    cout << "(  /  )" <<'\n';
+    cout << " \\(__)|" <<'\n';
+    return 0;
+}
