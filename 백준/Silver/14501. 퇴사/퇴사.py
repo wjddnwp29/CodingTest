f1112916f11b5f83db355dc257f67d1bd93a5408
정우제 <111894594+wjddnwp29@users.noreply.github.com>
2026-03-25 23:50:24 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,22 +1,22 @@
-'''
-N+1일째 되는날 퇴사 
-N일동안 최대한 많은 상담
-하루에 하나씩 서로 다른 상담
-T : 걸리는 기간 , P : 금액
-'''
-
 n = int(input())
-g = [list(map(int,input().split())) for _ in range(n)]
+t = [0] * (n+1)
+p = [0] * (n+1)
+for i in range(n):
+    a, b = map(int,input().split())
+    t[i] = a
+    p[i] = b
 
-answer = [0]
-def backtrack(day,money):
-    if day > n:
+ans = [0]
+
+def backtrack(idx,sum):
+    if idx > n:
         return
-    if day == n:
-        answer[0] = max(answer[0],money)
+    if idx == n:
+        ans[0] = max(ans[0],sum)
         return
-    
-    backtrack(day+g[day][0],money+g[day][1])
-    backtrack(day+1,money)
+
+    backtrack(idx+t[idx],sum+p[idx])
+    backtrack(idx+1,sum)
+
 backtrack(0,0)
-print(answer[0])
+print(ans[0])
