22b05e76432d06bcc3d69e11d292ad1de420553b
정우제 <111894594+wjddnwp29@users.noreply.github.com>
2026-05-08 10:49:58 +0900

    [Passed] 비교 연산 (47ms, 16MB)

---
+++ CodeTree/trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+
+print(1 if A >= B else 0)
+print(1 if A > B else 0)
+print(1 if B >= A else 0)
+print(1 if B > A else 0)
+print(1 if A == B else 0)
+print(1 if A != B else 0)
