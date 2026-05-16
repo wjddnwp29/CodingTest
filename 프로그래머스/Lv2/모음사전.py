daa88e0f2bfdbc4609d25d21bedf377bdb3349a3
wooje-jung <dnwpgmlfk@naver.com>
2026-05-16 19:29:41 +0900

    ADD : Lv2 모음사전

--- 프로그래머스/Lv2/모음사전.py
+++ 프로그래머스/Lv2/모음사전.py
@@ -1,21 +1,68 @@
+# word_list = ["A","E","I","O","U"]
+# temp = []
+# ans = 0
+
+# def backtrack(word):
+#     global ans
+#     if len(temp) == 5:
+#         return
+#     for i in word_list:
+#         temp.append(i)
+#         ans += 1
+#         if "".join(temp) == word:
+#             return
+#         backtrack(word)
+#         temp.pop()
+
+# def solution(word):
+#     backtrack(word)
+#     return ans
+
+
 word_list = ["A","E","I","O","U"]
-temp = []
-global ans_cnt
-ans_cnt = 0
+target = "AAAAE"
 
-def backtrack(n,word):
-    if n == 5:
-        ans = "".join(temp)
-        ans_cnt+=1
-        if word == ans:
-            print(ans_cnt)
+arr = []
+cnt = 0
+flag = False
+def dfs(depth):
+    global cnt, flag
+    if flag == True or depth  == 5:
+        return
+    
+    for i in range(len(word_list)):
+        cnt += 1
+        arr.append(word_list[i])
+        if target == "".join(arr):
+            flag = True
             return
-        else: 
+        dfs(depth+1)
+        if flag == True:
             return
-        
-    for i in word_list:
-        temp.append(i)
-        backtrack(n+1,word)
-        temp.pop()
+        arr.pop()
+dfs(0)  
+print(cnt)
 
-backtrack(0,"AAAAA")
+# word_list = ["A","E","I","O","U"]
+# target = "AAAAE"
+# arr = []
+# cnt = 0
+# found = False
+
+# def dfs(depth):
+#     global cnt, found
+#     if found or depth == 5:
+#         return
+#     for i in range(len(word_list)):
+#         cnt += 1
+#         arr.append(word_list[i])
+#         if target == "".join(arr):
+#             found = True
+#             return
+#         dfs(depth+1)
+#         if found:  # 찾았으면 pop 없이 바로 return
+#             return
+#         arr.pop()
+
+# dfs(0)
+# print(cnt)  # 6
