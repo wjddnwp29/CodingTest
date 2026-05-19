41d35765e6eb878b7fb37f1a171cab0ad04cb25c
wooje-jung <dnwpgmlfk@naver.com>
2026-05-19 21:17:04 +0900

    ADD : 코테복기 점수 카드 게임

---
+++ 코테복기/점수 카드 게임.py
@@ -0,0 +1,29 @@
+def apply(s, op, n):
+    if op == "+":
+        s += n
+        if s >= 10000: s -= 10000
+    elif op == "-":
+        s -= n
+        if s < 0: s += 10000
+    elif op == "*":
+        s *= n
+        if s >= 10000: s %= 10000
+    return s
+
+
+def solution(points):
+    dp = [False] * 10000
+    dp[0] = True
+    for a, b in points:
+        op1, n1 = a[0], int(a[1:])
+        op2, n2 = b[0], int(b[1:])
+        
+        ndp = [False] * 10000
+        for s in range(10000):
+            if dp[s]:
+                ndp[apply(s,op1,n1)] = True
+                ndp[apply(s,op2,n2)] = True
+        dp = ndp
+    scores = [s for s in range(10000) if dp[s]]
+            
+    return max(scores), min(scores)
